55b0f8deead23672339db39bdd9e2777e5da19fa
Aman Ullah Khan <amanullahkhan91929@gmail>
2023-06-07 12:20:23 +0500

    practice

---
+++ javascri[t.js
@@ -0,0 +1,59 @@
+// console.log(Increament);
+
+var count =10;
+console.log(count++);
+console.log(count++);
+console.log(count);
+
+// console.log(Decreament);
+
+var count =10;
+console.log(--count);
+console.log(count--);
+console.log(--count);
+
+
+// console.log(Increament,Decreament (Practice));
+
+var num1 = 8;
+var num2 = 4;
+var total= num2++ + ++num1 - ++num1 + ++num2;
+console.log(total);
+
+
+// console.log(Evencount (Practice));
+
+var evenCount = 6;
+console.log(evenCount);
+
+evenCount = evenCount + 2;
+console.log(evenCount);
+
+evenCount = evenCount + 5;
+console.log(evenCount);
+
+
+
+var userName = "Aman";
+
+userName = userName + " Ullah" + " Khan";
+
+console.log(userName)
+
+
+
+var num1 = 7;
+num1 = num1 - 3;
+
+console.log(num1);
+
+
+
+
+
+
+
+
+
+
+
